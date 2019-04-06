83653c4b5349f9f3624936e2ebcbb5db8941f380
Justin Brown <brownjustin@brownjustin-macbookpro.roam.corp.google.com>
2019-04-06 13:47:15 -0700

    Added js file

---
+++ js/bingo.js
@@ -0,0 +1,14 @@
+var icons = [
+  "icons/Tyrion Lannister.jpg",
+  "icons/Tyrion Lannister.jpg",
+  "icons/Tyrion Lannister.jpg"
+]
+
+
+for (var i = 0; i < icons.length; i++) {
+  var img = document.createElement("img");
+  img.src = icons[i];
+  var src = document.getElementById("img" + (i+1));
+  src.appendChild(img)
+}
+
