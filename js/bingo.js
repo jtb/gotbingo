a11a42971bb03c03a82ec0893cb654e1bdb81df9
Justin Brown <brownjustin@brownjustin-macbookpro.roam.corp.google.com>
2019-04-06 14:37:16 -0700

    fix up css

--- js/bingo.js
+++ js/bingo.js
@@ -29,6 +29,7 @@ var icons = [
 
 for (var i = 0; i < icons.length; i++) {
   var img = document.createElement("img");
+  img.className = 'tile';
   img.src = icons[i];
   var src = document.getElementById("img" + (i+1));
   src.appendChild(img)
